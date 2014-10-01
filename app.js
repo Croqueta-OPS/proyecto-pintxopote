9576f55f007351eabc0ebd7cc20753f5f7836d86
Artzai <aartxi@gmail.com>
2014-10-01 12:06:10 +0200

    majs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-	res.send('index.html');
+	res.send('public/index.html');
 });
 
 app.get('/pintxokk', function(req, res){

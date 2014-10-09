22841dc65a13e7bb3ffd2351f57935f0cd70beda
Artzai <aartxi@gmail.com>
2014-10-09 11:20:46 +0200

    heroku froga

--- app.js
+++ app.js
@@ -21,5 +21,8 @@ app.get('/pintxokek', function(req, res){
 
 // app.use('/', routes);
 
-app.listen(3000);
-console.log('Listening on port 3000');
+//app.listen(process.env.PORT || 3000);
+//console.log('Listening on port 3000');
+var server = app.listen(process.env.PORT || 3000, function(){
+    console.log('Listening in port %d', server.address().port);
+});

82e978ccb364eae443e0b60c02aa547d80318c17
borjamunoz <munoz.borja@gmail.com>
2014-10-24 12:00:35 +0200

    login insertado

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+	app.use(express.static(__dirname + '/public'));
 
+var conection = require('mongodb').MongoClient;
+var passport = require('passport');
+var flash 	 = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
 
 var pintxos = [];
 
-var conection = require('mongodb').MongoClient;
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 conection.connect("mongodb://croquetaOPS:Ladronmamon1@ds063879.mongolab.com:63879/croquetaops", function(err, db) {
 
@@ -28,19 +47,22 @@ conection.connect("mongodb://croquetaOPS:Ladronmamon1@ds063879.mongolab.com:6387
 
 });
 
-app.get('/', function (require, response) {
+//app.get('/', function (require, response) {
 
-	response.send('routes/index.html');
+	//response.send('routes/index.html');
 	
+//});
 
-});
+//app.get('/pintxos', function (require, response) {
 
-app.get('/pintxos', function (require, response) {
+	//response.json(pintxos);
 
-	response.json(pintxos);
+//});
 
-});
 
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 
 var server = app.listen(process.env.PORT || 3000, function() {
---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	'url':'mongodb://admin:admin@ds063929.mongolab.com:63929/login' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};

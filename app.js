d9ea77e0d43b7d188352707b5b6f7b1a8c03819c
borjamunoz <munoz.borja@gmail.com>
2014-10-28 09:13:26 +0100

    solucionado login local

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
 var express = require('express');
 var app = express();
 
-var conection = require('mongodb').MongoClient;
+//var conection = require('mongodb').MongoClient;
 
 var passport = require('passport');
 var flash 	 = require('connect-flash');
-//var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
-// 
-// var configDB = require('./config/database.js');
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url, function(err, res){
+		if(!err){
+			console.log('Conectado a bbdd');
+			
+			// var collection = db.collection('pintxos');
+
+ 		// 	collection.find().toArray(function(err, arrayPintxos) {
+
+			// 	arrayPintxos.forEach(function(element, index, array) {
+				
+					
+			// 		pintxos[pintxos.length] = element;
+					
+			// 		console.log(pintxos[pintxos.length-1]);		
+			// 	});
+
+			// });
+		}	
+}); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
 
 app.use(express.static(__dirname + '/public'));
 
@@ -24,40 +47,20 @@ app.use(bodyParser()); // get information from html forms
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: 'wendelmandanga' })); // session secret
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 	
-var configDB = require('./config/database.js');
 
 var pintxos = [];
 
-// configuration ===============================================================
-conection.connect(configDB.url, function(err, db){
-		if(!err){
-			console.log('Conectado a bbdd');
-			var collection = db.collection('pintxos');
 
- 			collection.find().toArray(function(err, arrayPintxos) {
+// app.get('/pintxos', function (require, response) {
 
-				arrayPintxos.forEach(function(element, index, array) {
-				
-					
-					pintxos[pintxos.length] = element;
-					
-					console.log(pintxos[pintxos.length-1]);		
-				});
+// 	response.json(pintxos);
 
-			});
-		}	
-}); // connect to our database
-
-app.get('/pintxos', function (require, response) {
-
-	response.json(pintxos);
-
-});
+// });
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport

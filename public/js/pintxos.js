f4917e3d5c9e5115f3bb7cd98d239ae50af0ae96
Artzai <aartxi@gmail.com>
2014-10-29 14:10:24 +0100

    formato al div pintxos

--- public/js/pintxos.js
+++ public/js/pintxos.js
@@ -7,13 +7,15 @@ $(document).ready(function() {
 		dataType: "json",
 		success: function(data){
 
-			var pintxos = "<h1>Pintxos</h1><h2>Degusta la variedad</h2>";
+			var pintxos = "<h1>Pintxos</h1><h2>Degusta la variedad</h2><div class='articulos'>";
 
-			data.forEach(function(element, index, array) {
-				
+			data.forEach(function(element, index, array) {				
+
 				pintxos += "<article><img src='img/croqueta.jpg'></img><h3>"+element.nombre+"</h3><p>"+element.descripcion+"</p></article>";					
 
-			});				
+			});		
+			
+			pintxos += "</div>";
 
 			$("#pintxos").html(pintxos);
 			

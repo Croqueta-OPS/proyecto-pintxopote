0bff1f0d67341f288140aade658adb7fb250642f
artzai <aartxi@gmail.com>
2014-10-30 16:41:13 +0000

    kk

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ require('./app/routes.js')(app, passport); // load our routes and pass in our ap
 
 
 //Servidor cloud9
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080, ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080 || 3000, ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 
 //Servidor OpenShift

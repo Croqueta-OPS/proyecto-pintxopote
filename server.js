41f12e965d2a4d9e0be76d59816e9eed195dffb3
artzai <aartxi@gmail.com>
2014-11-01 15:44:55 +0000

    kk

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ require('./app/routes.js')(app, passport); // load our routes and pass in our ap
 
 //servidor heroku
 var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
+
 app.listen(port, function() {
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+  console.log("running");
+})

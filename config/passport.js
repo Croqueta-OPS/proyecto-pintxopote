f3780227ede74f5a18264328ee62ddc0fc9dad67
artzai <aartxi@gmail.com>
2014-11-04 08:24:22 +0000

    login arreglado y gmaps arreglado

--- config/passport.js
+++ config/passport.js
@@ -74,13 +74,13 @@ module.exports = function(passport) {
         // by default, local strategy uses username and password, we will override with email
         usernameField : 'email',
         passwordField : 'password',
-        passReqToCallback : true , // allows us to pass back the entire request to the callback
+        passReqToCallback : true, // allows us to pass back the entire request to the callback
     },
     function(req, email, password, done) {
 
         // asynchronous
         // User.findOne wont fire unless data is sent back
-        process.nextTick(function() {
+        process.nextTick (function() {
 
 		// find a user whose email is the same as the forms email
 		// we are checking to see if the user trying to login already exists
@@ -96,25 +96,25 @@ module.exports = function(passport) {
 
 				// if there is no user with that email
                 // create the user
-                var newUser            = new User();
+                var newUser = new User();
 
                 // set the user's local credentials
-                newUser.local.email    = email;
+                newUser.local.email = email;
                 newUser.local.password = newUser.generateHash(password);
-                newUser.local.nomUsuario   = req.body.nombre;
+                newUser.local.nomUsuario = req.body.nombre;
+               
 
 				// save the user
-                newUser.save(function(err) {
+                newUser.save (function(err) {
                     if (err)
                         throw err;
-                    return done(null, newUser);
+                    return done (null, newUser);
                 });
             }
 
         });    
 
-        });
-
-    }));
-
+       
+    });
+}));
 };

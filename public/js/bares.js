c6cfa32cc1f1ac00cd5cb25409d4ee31a36e7cf3
artzai <aartxi@gmail.com>
2014-11-06 10:28:45 +0000

    probando rutas gmaps

--- public/js/bares.js
+++ public/js/bares.js
@@ -5,19 +5,27 @@ $(document).ready(function() {
 		type: "GET",
 		url: "/bares",
 		dataType: "json",
-		success: function(data){
+		success: function(data) {
+			
+			var directionsDisplay;
+			var directionsService = new google.maps.DirectionsService();
+			var map;
 
-		function initialize() {
+			function initialize() {
+				
+				directionsDisplay = new google.maps.DirectionsRenderer();
 				
 				var mapOptions = {
-					center:new google.maps.LatLng(43.3133505, -1.9782344),
-			        zoom:14,
+					center: new google.maps.LatLng(43.3133505, -1.9782344),
+			        zoom: 14,
 			        mapTypeControl: false,
 			        scrollwheel: false,
 			        mapTypeId:google.maps.MapTypeId.ROADMAP
-				}
+				};
 				
-				var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+				map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+				
+				directionsDisplay.setMap(map);
 				
 				var myLatlng;
 				
@@ -34,8 +42,31 @@ $(document).ready(function() {
 				});
 				
 			}
+			
+			function calcRoute() {
+				alert("kk");
 				
+				var start =  'Vielha e Mijaran, Lleida';
+				var end =  'La Massana, Andorra';
+				
+				var request = {
+					origin:start,
+					destination:end,
+					travelMode: google.maps.TravelMode.WALKING
+				};
+				
+				directionsService.route(request, function(response, status) {
+					
+					if (status == google.maps.DirectionsStatus.OK) {
+						directionsDisplay.setDirections(response);
+					}
+				
+				});
+			
+			}
+
 			google.maps.event.addDomListener(window, 'load', initialize);
+			calcRoute();
 				
 		}
 				

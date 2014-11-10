3cc0bf3daf558f9cee708b7f9320caeffda5f8cd
borjamunoz <munoz.borja@gmail.com>
2014-11-10 00:15:53 +0000

    primera ruta sencilla con origen, destino y 1 waypoint

--- public/js/cargarMarkers.js
+++ public/js/cargarMarkers.js
@@ -13,12 +13,45 @@ $.ajax({
     			
     			var marker = new google.maps.Marker({
     				position: myLatlng,
-    				map : map,
+    				//map : map,
     				title: element.nombre
     			});
     
     		});
-		
+    		
+            function calcRoute() {
+                
+                var start =  new google.maps.LatLng(data[1].coordX, data[1].coordY);
+                var end =  new google.maps.LatLng(data[2].coordX, data[2].coordY);
+                var waypoints = [];
+                
+                waypoints.push({
+                    location:new google.maps.LatLng(data[0].coordX, data[0].coordY),
+                    stopover:true
+                });
+                
+                var request = {
+                    origin:start,
+                    destination:end,
+                    waypoints: waypoints,
+                    travelMode: google.maps.TravelMode.WALKING
+                };
+                
+                directionsService.route(request, function(response, status) {
+                    
+                    if (status == google.maps.DirectionsStatus.OK) {
+                        
+                        directionsDisplay.setOptions({ preserveViewport: true });
+                        directionsDisplay.setDirections(response);
+                        
+                    }
+                    
+                });
+            
+            }
+            
+            calcRoute();
+
 	    }, 1000);
 		
 	}
--- public/js/googleMap.js
+++ public/js/googleMap.js
@@ -1,13 +1,21 @@
 var map;
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
 
 function initialize() {
 	
+	directionsDisplay = new google.maps.DirectionsRenderer();
+	
 	var mapOptions = {
 		zoom: 14,
+		minZoom: 14,
+		scrollwheel: false,
+		mapTypeControl: false,
 		center: new google.maps.LatLng(43.3133505, -1.9782344)
 	};
 	
 	map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+	directionsDisplay.setMap(map);
 
 }
 

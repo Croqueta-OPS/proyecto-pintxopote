75f15369410a89aca9e084d651aabcf4ee28e2ef
borjamunoz <munoz.borja@gmail.com>
2015-01-08 09:39:11 +0000

    1038

--- config/database.js
+++ config/database.js
@@ -1,6 +1,10 @@
 // config/database.js
+
+var user = process.env.USER;
+var password = process.env.PASS;
+
 module.exports = {
 
-	'url':'mongodb://process.env.USER:process.env.PASS@ds063879.mongolab.com:63879/croquetaops' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url':'mongodb://'+user+':'+password+'@ds063879.mongolab.com:63879/croquetaops' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
--- server.js
+++ server.js
@@ -43,9 +43,7 @@ require('./app/routes.js')(app, passport); // load our routes and pass in our ap
 
 //Servidor local ======================================================================
 /*var server = app.listen(process.env.PORT || 3000, function() {
-
     console.log('Listening in port %d', server.address().port);
-
 });*/
 
 
@@ -57,7 +55,6 @@ app.listen(port, ip);
 
 //servidor heroku
 /*var port = process.env.PORT || 5000; // Use the port that Heroku provides or default to 5000
-
 app.listen(port);*/
 
 console.log('Listening in port ' + port);

1ac54e7373b38cb662da8b6b009523083baccc94
borjamunoz <munoz.borja@gmail.com>
2015-02-05 10:20:16 +0000

    con websockets

--- server.js
+++ server.js
@@ -81,5 +81,48 @@ console.log('Listening in port ' + port);
 /////////TWITTER
 
 
+
+var watchList = ['pintxopote', 'pintxopo', 'pintxopotedonostia'];
+var follow = ['2872220943'];
+
+ var T = new Twit({
+    consumer_key:         'XhrMEQs2ne4u6wli0Hz9lsB65'
+  , consumer_secret:      'mVrSUMvmfpbfIkKIRklmSk14clFZxyOfbV9LYXTUu1lUO78U6X'
+  , access_token:         '2872220943-MQUfP0gpQm5I9tvug6MRdy3vSwctK4kWCv066Ac'
+  , access_token_secret:  'lJGWcN8OELnFiYJbJjjoJUjTVMrq3AA2VbR44OsezKZ7p'
+})
+
+io.sockets.on('connection', function (socket) {
+  console.log('Connected');
+
+
+ var stream = T.stream('statuses/filter', { follow : follow , track: watchList })
+
+  stream.on('tweet', function (tweet) {
+
+  	// Makes a link the Tweet clickable
+    var turl = tweet.text.match( /(http|https|ftp):\/\/[^\s]*/i )
+    if ( turl != null ) {
+      turl = tweet.text.replace( turl[0], '<a class="tweets" href="'+turl[0]+'" target="new">'+turl[0]+'</a>' );
+    } else {
+      turl = tweet.text;
+    }
+
+    var mediaUrl;
+    // Does the Tweet have an image attached?
+    if ( tweet.entities['media'] ) {
+      if ( tweet.entities['media'][0].type == "photo" ) {
+        mediaUrl = tweet.entities['media'][0].media_url;
+      } else {
+        mediaUrl = null;
+      }
+    }
+
+    // Send the Tweet to the browser
+    io.sockets.emit('stream',turl, tweet.user.screen_name, tweet.user.profile_image_url, mediaUrl);
+  
+
+  });
+ });
  
  ////////TWITTER

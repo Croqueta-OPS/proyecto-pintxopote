29b483be301b74504478a7e4a279d28a3c81af34
borjamunoz <munoz.borja@gmail.com>
2015-02-17 12:08:32 +0000

    Allow origin2

--- server.js
+++ server.js
@@ -58,6 +58,9 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', 'thepintxopo.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 

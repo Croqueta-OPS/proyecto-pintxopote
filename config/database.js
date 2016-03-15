9b78a346c460c17616195397d345ae59b9514f1c
borjamunoz <munoz.borja@gmail.com>
2016-03-15 21:48:05 +0000

    Arreglo URL mongolab

--- config/database.js
+++ config/database.js
@@ -5,6 +5,6 @@ var password = process.env.PASS1;
 
 module.exports = {
 
-	'url':'mongodb://'+user+':'+password+'@ds063879.mlab.com:63879/croquetaops' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url':'mongodb://croquetaOPS:Wendelmandanga1@ds063879.mlab.com:63879/croquetaops' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
